155d07a0850b8cc85b97a8279f10328f3a53e6d9
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-03-07 16:47:26 +0100

    included header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,26 +9,8 @@
     @vite('resources/js/app.js')
 </head>
 <body>
-    <header>
-        <section class="container">
-            <img src="{{ asset('img/dc-logo.png') }}" alt="dc-logo">
-            <div id="navbar">
-                <ul>
-                    <li><a href="{{route('home')}}">Home</a></li>
-                    <li><a href="{{url('/')}}">Characters</a></li>
-                    <li><a href="{{route('comics')}}">Comics</a></li>
-                    <li><a href="{{url('/')}}">TV</a></li>
-                    <li><a href="{{url('/')}}">Movies</a></li>
-                    <li><a href="{{url('/')}}">Games</a></li>
-                    <li><a href="{{url('/')}}">Collectibles</a></li>
-                    <li><a href="{{url('/')}}">Videos</a></li>
-                    <li><a href="{{url('/')}}">Fans</a></li>
-                    <li><a href="{{url('/')}}">News</a></li>
-                    <li><a href="{{url('/')}}">Shop</a></li>
-                </ul>
-            </div>
-        </section>
-    </header>
+    @include('includes.header');
+
     <main>
         <section id="jumbotron">
         </section>
@@ -41,19 +23,70 @@
         <div class="bottom-main">
             <div class="container">
                 <ul>
-                    <li class="navbar-main"><img src="../assets/img/buy-comics-digital-comics.png" alt="digital"><a
+                    <li class="navbar-main"><img src="{{ asset('img/buy-comics-digital-comics.png') }}" alt="digital"><a
                             href="#">Digital comics</a></li>
-                    <li class="navbar-main"><img src="../assets/img/buy-comics-merchandise.png" alt="merchandise"><a
+                    <li class="navbar-main"><img src="../img/buy-comics-merchandise.png" alt="merchandise"><a
                             href="#">dc merchandise</a></li>
-                    <li class="navbar-main"><img src="../assets/img/buy-comics-subscriptions.png" alt="sub"><a
+                    <li class="navbar-main"><img src="../img/buy-comics-subscriptions.png" alt="sub"><a
                             href="#">Subscription</a></li>
-                    <li class="navbar-main"><img src="../assets/img/buy-comics-shop-locator.png" alt="lot"><a href="#">Comic
+                    <li class="navbar-main"><img src="../img/buy-comics-shop-locator.png" alt="lot"><a href="#">Comic
                             shop locator</a></li>
-                    <li class="navbar-main"><img src="../assets/img/buy-dc-power-visa.svg" alt="via"><a href="#">Dc power
+                    <li class="navbar-main"><img src="../img/buy-dc-power-visa.svg" alt="via"><a href="#">Dc power
                             visa</a></li>
                 </ul>
             </div>
         </div>
     </main>
+    <footer>
+        <div class="container">
+            <div class="links-footer">
+                <div class="links-col">
+                    <h3>dc comics</h3>
+                    <ul>
+                        <li><a href="#">Characters</a></li>
+                        <li><a href="#">Comics</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">Games</a></li>
+                        <li><a href="#">Videos</a></li>
+                        <li><a href="#">News</a></li>
+                    </ul>
+                    <h3>Shop</h3>
+                    <ul>
+                        <li><a href="#">Shop DC</a></li>
+                        <li><a href="#">Shop DC Collectibles</a></li>
+                    </ul>
+                </div>
+                <div class="links-col">
+                    <h3>dc comics</h3>
+                    <ul>
+                        <li><a href="#">Terms Of Use</a></li>
+                        <li><a href="#">Privacy</a></li>
+                        <li><a href="#">Ad Choices</a></li>
+                        <li><a href="#">Advertising</a></li>
+                        <li><a href="#">Jobs</a></li>
+                        <li><a href="#">Subscriptions</a></li>
+                        <li><a href="#">Talents</a></li>
+                        <li><a href="#">CPSC</a></li>
+                        <li><a href="#">Ratings</a></li>
+                        <li><a href="#">Shop Help</a></li>
+                    </ul>
+                </div>
+                <div class="links-col">
+                    <h3>sites</h3>
+                    <ul>
+                        <li><a href="#">DC</a></li>
+                        <li><a href="#">MAD Magazine</a></li>
+                        <li><a href="#">DC Kids</a></li>
+                        <li><a href="#">DC Universe</a></li>
+                        <li><a href="#">DC Power Visa</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="img-footer">
+                <!-- <img src="../assets/img/dc-logo-bg.png" alt=""> -->
+            </div>
+        </div>
+    </footer>
 </body>
 </html>
