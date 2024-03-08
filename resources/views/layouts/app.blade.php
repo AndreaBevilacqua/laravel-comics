c8c92afd5b83e37e4b18798c009df2914e776230
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-03-08 16:49:18 +0100

    fixed code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{$title}} | @yield('title')</title>
+    <title>{{env('APP_NAME', 'DC Comics')}} | @yield('title')</title>
 
     {{-- Vite --}}
     @vite('resources/js/app.js')
 
     {{-- Cdns --}}
     @yield('cdns')
-    
+
 </head>
 <body>
     {{-- Header --}}
